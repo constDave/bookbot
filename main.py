023b4b5287bcc86ef58b0f9995f4dc1093bbc122
constDave <const.dave@gmail.com>
2024-08-21 15:56:20 -0800

    added letter count in the report

--- main.py
+++ main.py
@@ -3,11 +3,34 @@ def count_words(file_contents):
     return len(words)
 
 
+def read_file(file_path):
+    with open(file_path, "r") as f:
+        return f.read()
+
+
+def count_characters(file_contents):
+    words = file_contents.split()
+    characters_count = {}
+    for word in words:
+        word = word.lower()
+        for char in word:
+            if char in characters_count:
+                characters_count[char] += 1
+            else:
+                characters_count[char] = 1
+    return characters_count
+
+
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        word_count = count_words(file_contents)
-        print(f"There are {word_count} words in this file.")
+    file_path = "books/frankenstein.txt"
+    file_contents = read_file(file_path)
+    word_count = count_words(file_contents)
+    characters_count = count_characters(file_contents)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print("Letter occurrences:\n")
+    print(characters_count)
+    print("\n\n")
+    print(f"There are {word_count} words in this file.")
 
 
 if __name__ == "__main__":
